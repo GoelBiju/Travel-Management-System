73f4930ef236addd9fb71404c05f2ebe782f4091
(s) Andrew Bellas <andrew.bellas@students.plymouth.ac.uk>
2019-02-20 15:18:22 +0000

    Created ROUTES table create statement

---
+++ Database/SQL Create Statements/cr_ROUTES.sql
@@ -0,0 +1,6 @@
+CREATE TABLE ROUTES 
+(
+    route_id            VARCHAR(4) NOT NULL PRIMARY KEY,
+    start_destination   VARCHAR(112) NOT NULL,
+    end_destination     VARCHAR(112) NOT NULL  
+);

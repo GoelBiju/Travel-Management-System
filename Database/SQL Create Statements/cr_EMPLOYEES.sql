0be4335224ea8c32c356387e9f129edace99693c
(s) Andrew Bellas <andrew.bellas@students.plymouth.ac.uk>
2019-02-20 15:28:43 +0000

    Created EMPLOYEES table create statement

---
+++ Database/SQL Create Statements/cr_EMPLOYEES.sql
@@ -0,0 +1,8 @@
+CREATE TABLE EMPLOYEES
+(
+    employee_id         VARCHAR(112) NOT NULL PRIMARY KEY,
+    first_name          VARCHAR(112) NOT NULL,
+    last_name           VARCHAR(112) NOT NULL,
+    job_role            VARCHAR(112) NOT NULL,
+    password            VARCHAR(112) NOT NULL     
+);

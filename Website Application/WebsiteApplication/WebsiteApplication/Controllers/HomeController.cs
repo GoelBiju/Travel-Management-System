69572c4bb3f1830757209001941bd9ba04584cfb
(s) Andrew Bellas <andrew.bellas@students.plymouth.ac.uk>
2019-03-06 16:13:25 +0000

    Added styling to login on website MVC

--- Website Application/WebsiteApplication/WebsiteApplication/Controllers/HomeController.cs
+++ Website Application/WebsiteApplication/WebsiteApplication/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace WebsiteApplication.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login";
+
+            return View();
+        }
     }
 }

e1bf12190e8af56bb8efe4b8a2d950e294459f55
(s) Andrew Bellas <andrew.bellas@students.plymouth.ac.uk>
2019-03-06 16:18:17 +0000

    Added help function pop up window in Javascript for web MVC login

---
+++ Website Application/WebsiteApplication/WebsiteApplication/Scripts/HelpFunctionScript.js
@@ -0,0 +1,5 @@
+﻿//JavaScript source code
+
+function HelpFunction() {
+    window.alert("Please enter your login information in the fields provided. This should be your employee ID and your password. You should then click the 'login' button and wait for the authentication process to occur.");
+}

912e94c31c08271abb23ad8bc8bf983d84919e80
wjbutler <wjbutler@bitbucket.org>
2019-03-11 15:05:55 +0000

    Added connection code to get employee details

---
+++ Website Application/WebsiteApplication/WebsiteApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using WebsiteApplication.Models;
+
+namespace WebsiteApplication.Controllers
+{
+    public class EmployeeController
+    {
+        static async Task RunAsync(HttpClient client)
+        {
+            client.BaseAddress = new Uri("http://web.socem.plymouth.ac.uk/IntProj/PRCS252E/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var StatusCode = await GetAllEmployees(client);
+        }
+        static async Task<HttpStatusCode> GetAllEmployees(HttpClient client)
+        {
+            HttpResponseMessage response = await client.GetAsync("employees");
+            if (response.IsSuccessStatusCode)
+            {
+                var JsonString = await response.Content.ReadAsStringAsync();
+                var _Data = JsonConvert.DeserializeObject<List<employee>>(JsonString);
+                foreach (employee e in _Data)
+                {
+                    Console.WriteLine(e.GetFirstName());
+                }
+            }
+
+            return response.StatusCode;
+        }
+    }
+}
---
+++ Website Application/WebsiteApplication/WebsiteApplication/Models/employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteApplication.Models
+{
+    public class employee
+    {
+        private string id { get; set; }
+        public string firstName { get; set; }
+        private string lastName { get; set; }
+        private string jobRole { get; set; }
+        private string password { get; set; }
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+    }
+}

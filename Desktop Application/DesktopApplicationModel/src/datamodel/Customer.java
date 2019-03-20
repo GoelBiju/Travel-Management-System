6037b6786a08010c007a78125d372f02197cf2e1
(s) Goel Biju <goel.biju@students.plymouth.ac.uk>
2019-03-20 01:21:00 +0000

    Added example converting Customer JSON to a Java object in desktop application.

---
+++ Desktop Application/DesktopApplicationModel/src/datamodel/Customer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datamodel;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Goel
+ */
+public class Customer {
+    
+    public int customerId;
+    
+    public String firstName;
+    
+    public String lastName;
+    
+    public Date dateOfBirth;
+    
+    public String addressLineOne;
+    
+    public String addressLineTwo;
+    
+    public String postCode;
+    
+    public String mobileNumber;
+    
+    public String emailAddress;
+    
+}
--- Desktop Application/DesktopApplicationModel/src/utilities/APIConnection.java
+++ Desktop Application/DesktopApplicationModel/src/utilities/APIConnection.java
@@ -5,20 +5,23 @@
  */
 package utilities;
 
-//import com.fasterxml.jackson.databind.DeserializationFeature;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
+//import java.io.BufferedReader;
+//import java.io.ByteArrayOutputStream;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
+//import java.net.HttpURLConnection;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+
 import java.net.URL;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
+
+import datamodel.Customer;
+
+
 
 /**
  *
@@ -26,46 +29,74 @@ import com.fasterxml.jackson.databind.*;
  */
 public class APIConnection {
     
- public static ObjectMapper mapper = new ObjectMapper().
-	configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).enable(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT);
+    public static ObjectMapper mapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).enable(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT);
     
-    public static Object[] GetData(String endPoint, Class tableClass)
-    {
-        Object[] object = null;
-        try 
-        {
-		URL url = new URL("http://web.socem.plymouth.ac.uk/IntProj/PRCS252E/api" + "/" + endPoint);
-		
-		object = (Object[]) mapper.readValue(url, tableClass);
-	} 
-        catch (IOException e)
-        {
-              e.printStackTrace();
-        }
-        return object;
-    }
+//    public static Object[] GetData(String endPoint, Class tableClass)
+//    {
+//        Object[] object = null;
+//        try 
+//        {
+//		URL url = new URL("http://web.socem.plymouth.ac.uk/IntProj/PRCS252E/api" + "/" + endPoint);
+//		
+//		object = (Object[]) mapper.readValue(url, tableClass);
+//	} 
+//        catch (IOException e)
+//        {
+//              e.printStackTrace();
+//        }
+//        return object;
+//    }
+//    
+//    public static Object getData(String tableName, Class tableClass, Integer id) {
+//        Object object = new Object();
+//        
+//        try {
+//            URL url = new URL("http://xserve.uopnet.plymouth.ac.uk/Modules/INTPROJ/PRCS251C/api/" + tableName + "/" + id);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//		conn.setRequestMethod("GET");
+//		conn.setRequestProperty("Accept", "application/json");
+//
+//		if (conn.getResponseCode() != 200) {
+//			throw new RuntimeException("Failed : HTTP error code : "
+//					+ conn.getResponseCode());
+//		}
+//
+//		BufferedReader br = new BufferedReader(new InputStreamReader(
+//			(conn.getInputStream())));           
+//        } catch (IOException | RuntimeException ex) {
+//            ex.printStackTrace();
+//        }
+//        
+//        return object;
+//    }
     
-    public static Object getData(String tableName, Class tableClass, Integer id) {
+    /**
+     * Example showing how to retrieve a single Customer object as a test 
+     * and convert from JSON to a Java object.
+     * @return 
+     */
+    public static Object getCustomer() {
         Object object = new Object();
         
         try {
-            URL url = new URL("http://xserve.uopnet.plymouth.ac.uk/Modules/INTPROJ/PRCS251C/api/" + tableName + "/" + id);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Accept", "application/json");
-
-		if (conn.getResponseCode() != 200) {
-			throw new RuntimeException("Failed : HTTP error code : "
-					+ conn.getResponseCode());
-		}
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-			(conn.getInputStream())));           
-        } catch (IOException | RuntimeException ex) {
+            String uri = "http://web.socem.plymouth.ac.uk/IntProj/PRCS252E/api/customers/11";
+            URL url = new URL(uri);
+        
+            object = mapper.readValue(url, Customer.class);
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         
+        
         return object;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("APIConnection test.");
+        
+        Customer customer = (Customer) getCustomer();
+        System.out.println("First Name: " + customer.firstName);
     }
     
 }

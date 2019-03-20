bbc88cda7da54a3aa815b120fffbb7a780463901
wjbutler <wjbutler@bitbucket.org>
2019-03-20 11:05:33 +0000

    added jar files to the file directory

--- Desktop Application/DesktopApplicationModel/src/utilities/APIConnection.java
+++ Desktop Application/DesktopApplicationModel/src/utilities/APIConnection.java
@@ -18,9 +18,7 @@ import java.net.URL;
 
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
-
-import datamodel.Customer;
-import datamodel.Employee;
+import datamodel.*;
 import java.io.IOException;
 
 
@@ -49,33 +47,11 @@ public class APIConnection {
         }
        return object;
     }   
-//    public static Object getData(String tableName, Class tableClass, Integer id) {
-//        Object object = new Object();
-//        
-//        try {
-//            URL url = new URL("http://xserve.uopnet.plymouth.ac.uk/Modules/INTPROJ/PRCS251C/api/" + tableName + "/" + id);
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//		conn.setRequestMethod("GET");
-//		conn.setRequestProperty("Accept", "application/json");
-//
-//		if (conn.getResponseCode() != 200) {
-//			throw new RuntimeException("Failed : HTTP error code : "
-//					+ conn.getResponseCode());
-//		}
-//
-//		BufferedReader br = new BufferedReader(new InputStreamReader(
-//			(conn.getInputStream())));           
-//        } catch (IOException | RuntimeException ex) {
-//            ex.printStackTrace();
-//        }
-//        
-//        return object;
-//    }
     
     /**
      * Example showing how to retrieve a single Customer object as a test 
      * and convert from JSON to a Java object.
-     * @return 
+     * @return Customer object mapped from json data in the database
      */
     public static Object getCustomer() {
         Object object = new Object();

fd4b618c704b074ca3d9b7b55430eac687984f5a
(s) Vincent Castellani <vincent.castellani@students.plymouth.ac.uk>
2019-03-21 16:52:41 +0000

    Started the website from fresh, currently focusing on getting employee to work, still needs more work

---
+++ Website Application/WebApplication/WebApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //https://www.tutorialsteacher.com/mvc/model-binding-in-asp.net-mvc
+
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetById(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            return View();
+        }
+    }
+}
---
+++ Website Application/WebApplication/WebApplication/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Employee
+    {
+        public string EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobRole { get; set; }
+        public string Password { get; set; }
+    }
+}

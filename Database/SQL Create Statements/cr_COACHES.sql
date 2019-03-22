009bdc229c121e90d67e5bff45be093025385d9d
(s) Andrew Bellas <andrew.bellas@students.plymouth.ac.uk>
2019-03-22 16:20:40 +0000

    Reworked coaches table with constraints and changed field names

--- Database/SQL Create Statements/cr_COACHES.sql
+++ Database/SQL Create Statements/cr_COACHES.sql
@@ -1,9 +1,21 @@
 CREATE TABLE COACHES
 (
-    coach_id INT NOT NULL PRIMARY KEY,
-    coach_make VARCHAR(35) NOT NULL,
-    coach_model VARCHAR(35) NOT NULL,
-    registration_plate VARCHAR(8) NOT NULL,
-    capacity INT NOT NULL
-    status VARCHAR(12) NOT NULL
+    coach_id INT
+    CONSTRAINT coaches_coach_id PRIMARY KEY,
+    
+    coach_make VARCHAR(35)
+    CONSTRAINT coaches_coach_make_notnull NOT NULL,
+    
+    coach_model VARCHAR(35)
+    CONSTRAINT coaches_coach_model_notnull NOT NULL,
+    
+    registration_plate VARCHAR(8)
+    CONSTRAINT coaches_registration_notnull NOT NULL,
+    CONSTRAINT coaches_registration_unique UNIQUE,
+    
+    coach_capacity INT
+    CONSTRAINT coaches_coach_capacity_notnull NOT NULL,
+    
+    active_status VARCHAR(12)
+    CONSTRAINT coaches_active_status_notnull NOT NULL
 );

739cb4cb90ed3101a6aa1551223f0b8970126cbd
Andrew Bellas <adbellas@socnet.soc.plymouth.ac.uk>
2019-03-27 11:39:33 +0000

    Added new binding controller to employee login on API

---
+++ Middleware/api/api/Models/BindingModels/EmployeeLoginBindingModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models.BindingModels
+{
+    public class EmployeeLoginBindingModel
+    {
+        //For employee ID entered:
+        [JsonProperty("employeeID")]
+        [Required]
+        public string employeeID { get; set; }
+
+        //For employee password entered:
+        [JsonProperty("password")]
+        [Required]
+        public string password { get; set; }
+    }
+}

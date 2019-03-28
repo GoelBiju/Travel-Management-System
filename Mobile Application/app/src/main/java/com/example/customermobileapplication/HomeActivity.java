6d51740ed8c17ec722bd3ff54b333ca04461870c
wjbutler <wjbutler@bitbucket.org>
2019-03-28 13:31:37 +0000

    Added Calender view to home page date search

--- Mobile Application/app/src/main/java/com/example/customermobileapplication/HomeActivity.java
+++ Mobile Application/app/src/main/java/com/example/customermobileapplication/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.customermobileapplication;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +19,8 @@ import com.example.customermobileapplication.Utilities.APIResponse;
 
 import org.w3c.dom.Text;
 
+import java.util.Calendar;
+
 public class HomeActivity extends AppCompatActivity {
 
     private int customerId;
@@ -25,6 +29,7 @@ public class HomeActivity extends AppCompatActivity {
     private EditText editTextTime;
     private TextView requestText;
     private APIConnection apiConnection;
+    private DatePickerDialog datePickerDialog;
 
     // Buttons.
     private Button viewAccountButton;
@@ -44,12 +49,14 @@ public class HomeActivity extends AppCompatActivity {
         BindView();
 
         setViewActions();
+        prepareDatePickerDialog();
 
     }
 
 
     private void BindView(){
         this.viewAccountButton = findViewById(R.id.buttonViewAccount);
+        this.editTextDate = findViewById(R.id.editTextDate);
     }
 
 
@@ -64,5 +71,25 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(viewAccountDetailsIntent);
             }
         });
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    private void prepareDatePickerDialog() {
+        // Get current date.
+        Calendar calendar = Calendar.getInstance();
+
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                editTextDate.setText(day + "/" + (month + 1) + "/" + year);
+                datePickerDialog.dismiss();
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
     }
 }

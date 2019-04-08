67cc753c3048f333adf4b84e8308e86ebd9da241
(s) Vincent Castellani <vincent.castellani@students.plymouth.ac.uk>
2019-04-08 12:21:56 +0100

    Added journey create statement

--- Database/SQL Create Statements/cr_JOURNEYS.sql
+++ Database/SQL Create Statements/cr_JOURNEYS.sql
@@ -10,9 +10,39 @@
         - coach_id;
         
         - employee_id;
-        
+
+	- start_station
+	
+	- end_station        
+
         - start_time/date;
         
         - end_time/date;
 */
+
+
+CREATE TABLE journeys(
     
+    journey_id NUMBER NOT NULL
+        CONSTRAINT journeys_journey_id_pk PRIMARY KEY,
+  
+    route_id NUMBER NOT NULL
+        CONSTRAINT routes_route_id_fk REFERENCES routes(route_id),
+        
+    coach_id NUMBER NOT NULL
+        CONSTRAINT coaches_coach_id_fk REFERENCES coaches(coach_id),
+        
+    employee_id VARCHAR(15) NOT NULL
+        CONSTRAINT employees_employee_id_fk REFERENCES employees(employee_id),
+        
+    start_station NUMBER NOT NULL    
+        CONSTRAINT stations_start_station_id_fk REFERENCES stations(station_id),
+        
+    end_station NUMBER NOT NULL
+        CONSTRAINT stations_end_station_id_fk REFERENCES stations(station_id),
+        
+    start_date_time DATE NOT NULL,
+    
+    end_date_time DATE NOT NULL
+    
+);

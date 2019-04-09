17dd8b2e61496c4596311fb46f7bcbf07d28824a
(s) William Butler <william.butler@students.plymouth.ac.uk>
2019-04-09 11:34:46 +0100

    Added journeyDTO and binding model classes

---
+++ Middleware/api/api/Models/BindingModels/JourneyBindingModel.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api.Models.BindingModels
+{
+    public class JourneyBindingModel
+    {
+        [JsonProperty("coachId")]
+        [JsonRequired]
+        public int CoachId { get; set; }
+
+        [JsonProperty("coachMake")]
+        [JsonRequired]
+        public string CoachMake { get; set; }
+
+        [JsonProperty("coachModel")]
+        [JsonRequired]
+        public string CoachModel { get; set; }
+
+        [JsonProperty("registrationPlate")]
+        [JsonRequired]
+        public string RegistratonPlate { get; set; }
+
+        [JsonProperty("coachCapacity")]
+        [JsonRequired]
+        public int CoachCapacity { get; set; }
+
+        [JsonProperty("isActive")]
+        [JsonRequired]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Middleware/api/api/Models/DTO/JourneyDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace api.Models.DTO
+{
+    public class JourneyDTO
+    {
+        [JsonProperty("journeyid")]
+        public int JourneyId { get; set; }
+
+        
+        [JsonProperty("routeid")]
+        public int routeId { get; set; }
+
+        
+        [JsonProperty("coachid")]
+        public int CoachId { get; set; }
+
+        
+        [JsonProperty("employeeid")]
+        public int EmployeeId { get; set; }
+
+        
+        [JsonProperty("startdatetime")]
+        public string DepartureTime { get; set; }
+
+        
+        [JsonProperty("enddatetime")]
+        public string ArrivalTime { get; set; }
+    }
+}

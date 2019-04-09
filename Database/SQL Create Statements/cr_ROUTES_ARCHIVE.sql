f9744c2f228aeee814899bfe49728a1ee13ff870
(s) William Butler <william.butler@students.plymouth.ac.uk>
2019-04-09 11:48:14 +0100

    Added Create statement for route archive table which is in the database

---
+++ Database/SQL Create Statements/cr_ROUTES_ARCHIVE.sql
@@ -0,0 +1,23 @@
+/*
+    PRCS252E - Travel Management System - CREATE routes_archive
+    
+    Attributes and relations:
+    
+        - route_id;
+        
+        - departure_station;
+        
+        - arrival_station;
+*/
+
+CREATE TABLE routes_archive(
+
+    route_id NUMBER 
+        CONSTRAINT routes_archive_route_id_pk PRIMARY KEY,
+
+    departure_station NUMBER
+        CONSTRAINT routes_archive_departure_station_fk REFERENCES stations (station_id),
+
+    arrival_station NUMBER
+        CONSTRAINT routes_archive_arrival_station_fk REFERENCES stations (station_id)
+);

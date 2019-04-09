8f3e0d9cb56d34c6a8b29cd8edb54aef4d62d59b
(s) William Butler <william.butler@students.plymouth.ac.uk>
2019-04-09 11:29:15 +0100

    Added model and controller for journeys to web application

---
+++ Website Application/WebApplication/WebApplication/ViewModels/JourneyViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WebApplication.ViewModels
+{
+    public class JourneyViewModel
+    {
+        [Display(Name = "Journey ID")]
+        public int JourneyId { get; set; }
+
+        [Display(Name = "Route ID")]
+        [JsonProprty("routeid")]
+        public int routeId { get; set; }
+
+        [Display(Name = "Coach ID")]
+        [JsonProprty("coachid")]
+        public int CoachId { get; set; }
+
+        [Display(Name = "Employee ID")]
+        [JsonProprty("employeeid")]
+        public int EmployeeId { get; set; }
+
+        [Display(Name = "Departure Time")]
+        [JsonProperty("startdatetime")]
+        public string DepartureTime { get; set; }
+
+        [Display(Name = "Arrival Time")]
+        [JsonProperty("enddatetime")]
+        public string ArrivalTime { get; set; }
+    }
+}

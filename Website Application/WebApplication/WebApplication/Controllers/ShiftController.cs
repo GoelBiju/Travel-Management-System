4910f51eb86cd866c3276e74266ee9fb08a25dfa
(s) Vincent Castellani <vincent.castellani@students.plymouth.ac.uk>
2019-04-10 11:58:04 +0100

    Started shift controller, view model, details view - currently dont work

---
+++ Website Application/WebApplication/WebApplication/Controllers/ShiftController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using WebApplication.ViewModels;
+using System.Web.Mvc;
+
+
+namespace WebApplication.Controllers
+{
+    public class ShiftController : Controller
+    {
+        // GET: Shift
+        public ActionResult Index()
+        {
+            var _Data = new List<ShiftViewModel>();
+
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("shifts").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var JsonString = response.Content.ReadAsStringAsync().Result;
+                _Data = JsonConvert.DeserializeObject<List<ShiftViewModel>>(JsonString);
+            }
+
+            return View(_Data);
+        }
+
+        //GET: Shift/Details/shiftId
+        public ActionResult Details(int id)
+        {
+            var _Data = new CoachViewModel();
+
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("shifts/" + id.ToString()).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var JsonString = response.Content.ReadAsStringAsync().Result;
+                _Data = JsonConvert.DeserializeObject<CoachViewModel>(JsonString);
+            }
+            return View(_Data);
+        }
+    }
+}
---
+++ Website Application/WebApplication/WebApplication/ViewModels/ShiftViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.ViewModels
+{
+    public class ShiftViewModel
+    {
+        
+        [Display(Name = "Shift ID")]
+        public int ShiftId { get; set; }
+
+        [Display(Name = "Employee ID")]
+        [JsonProperty("employeeid")]
+        public int EmployeeId { get; set; }
+
+        [Display(Name = "Route ID")]
+        [JsonProperty("routeid")]
+        public int RouteId { get; set; }
+
+
+        [Display(Name = "Coach ID")]
+        [JsonProperty("coachid")]
+        public int CoachId { get; set; }
+
+
+        [Display(Name = "Start of Shift")]
+        [JsonProperty("startofshift")]
+        public int StartOfShift { get; set; }
+
+        [Display(Name = "End of Shift")]
+        [JsonProperty("endofshift")]
+        public int EndOfShift { get; set; }
+
+    }
+}

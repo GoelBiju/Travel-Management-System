afafe6560c367347e79a722a49dde552c449f55e
Andrew Bellas <adbellas@SOCNET.SOC.PLYMOUTH.AC.UK>
2019-04-12 17:13:13 +0100

    Added retrieval of routes for admin on web app

---
+++ Website Application/WebApplication/WebApplication/Controllers/RoutesController.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.ViewModels;
+
+namespace WebApplication.Controllers
+{
+    public class RoutesController : Controller
+    {
+        // GET: Routes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Details()
+        {
+            var data = new List<RoutesModel>();
+
+
+            HttpResponseMessage responseMessage = GlobalVariables.WebApiClient.GetAsync("routes").Result;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var JsonString = responseMessage.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject<List<RoutesModel>>(JsonString);
+            }
+
+
+            return View(data);
+        }
+    }
+}
---
+++ Website Application/WebApplication/WebApplication/ViewModels/RoutesModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.ViewModels
+{
+    public class RoutesModel
+    {
+        [Display(Name = "route id")]
+        public int routeId { get; set; }
+
+        [Display(Name = "departure station")]
+        public int departureStation { get; set; }
+
+        [Display(Name = "arrival station")]
+        public int arrivalStation { get; set; }
+
+    }
+}

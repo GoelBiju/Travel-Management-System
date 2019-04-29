b8cc4a5d3374f7dc0686824c61ccf8ca4bb8844b
(s) Goel Biju <goel.biju@students.plymouth.ac.uk>
2019-04-29 16:13:00 +0100

    Fixed: Previous bug fixed with published API.

--- Mobile Application/app/src/main/java/com/example/customermobileapplication/BookingActivity.java
+++ Mobile Application/app/src/main/java/com/example/customermobileapplication/BookingActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -141,6 +142,8 @@ public class BookingActivity extends AppCompatActivity {
         apiConnection.getCustomJsonObject("journeys/" + this.journeyId, Journey.class, new CustomCallback() {
             @Override
             public void onSuccess(Object responseObject) {
+                Log.d("Response", "Successfully received journey object.");
+
                 // Get the journey object.
                 Journey journey = (Journey) responseObject;
 

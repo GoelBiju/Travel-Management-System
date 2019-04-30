5734ef56c4a233333834fb0fa33d1f946a4fbf24
(s) Andrew Bellas <andrew.bellas@students.plymouth.ac.uk>
2019-04-30 16:16:08 +0100

    Added help function script and fixed help button on login page

---
+++ Website Application/WebApplication/WebApplication/Scripts/LoginHelpFunction.js
@@ -0,0 +1,3 @@
+﻿function loginHelp() {
+    alert("Please make sure to enter a valid employee ID and password into the respective fields. Then, press the login button and wait for the login process to complete.")
+}
